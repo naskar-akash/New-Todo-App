604c34a3e51ad5d2158720d24dfb974bff078ad8
naskar-akash <naskar.akash1947@gmail.com>
2025-08-07 17:19:38 +0530

    improving user registrationn form

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,37 +5,57 @@ function App() {
   const {
     register,
     handleSubmit,
-    formState: { errors },
+    formState: { errors, isSubmitting },
   } = useForm();
-  const onSubmit = (data) => console.log(data);
+
+  //delay function
+  const delay = (d) => {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve()
+      }, d*1000);
+    })
+  };
+
+  //submitting
+  const onSubmit = async (data) => {
+    await delay(2);
+    console.log(data);
+  };
 
   return (
-    <div>
+    <div className="flex flex-col">
+      {isSubmitting?<div className="text-xl mx-3 my-2">Creating user...</div>:<div className="text-xl mx-3 my-2">Create user</div>}
+
       <form
         className="flex flex-col gap-5 w-2/3 justify-center mt-10 font-serif"
         onSubmit={handleSubmit(onSubmit)}
       >
         <input
           className="bg-zinc-300 px-3 py-2 mx-3 rounded-md"
-          defaultValue="todo title"
-          {...register("todoTitle")}
+          placeholder="fullname"
+          {...register("fullname", {required: {value: true, message: "This field is required"}})}
+          type="text"
         />
+        {errors.fullname && <div className="text-red-500 text-sm mt-2">{errors.fullname.message}</div> }
         <input
           className="bg-zinc-300 px-3 py-2 mx-3 rounded-md"
-          defaultValue="todo description"
-          {...register("todoDesc")}
+          placeholder="email"
+          {...register("email", {required: {value: true, message: "This field is required"}})}
+          type="email"
         />
-        <label className="text-lg mx-3">Choose status:</label>
-        <select className="bg-zinc-300 px-3 py-2 mx-3 rounded-md" {...register("status")}>
-        <option value="open">open</option>
-        <option value="ongoing">ongoing</option>
-        <option value="done">done</option>
-        <option value="unseen">unseen</option>
-      </select>
-
+        {errors.email && <div className="text-red-500 text-sm mt-2">{errors.email.message}</div> }
         <input
-          className="bg-blue-500 text-white px-3 py-2 mx-3 text-sm rounded-md"
+          className="bg-zinc-300 px-3 py-2 mx-3 rounded-md"
+          placeholder="password"
+          {...register("password", {required: {value: true, message: "This field is required"}, minLength: {value: 8, message: "Password should contain minimum 8 characters"}})}
+          type="password"
+        />
+        {errors.password && <div className="text-red-500 text-sm mt-2">{errors.password.message}</div> }
+        <input
+          className="bg-blue-500 text-white px-3 py-2 mx-3 text-sm rounded-md disabled:bg-blue-300 disabled:cursor-not-allowed"
           type="submit"
+          disabled={isSubmitting}
         />
       </form>
     </div>
