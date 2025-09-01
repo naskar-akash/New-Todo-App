90654ba25c6711b5c7d242b4e022a02ae493e7f0
naskar-akash <naskar.akash1947@gmail.com>
2025-09-01 22:54:45 +0530

    Completing filter functionality

--- server/utils/dateTime.js
+++ server/utils/dateTime.js
@@ -1,13 +1,13 @@
 const  dateTime = () => {
   const dateObj = new Date();
 
-  const dd = String(dateObj.getDate()).padStart(2, "");
-  const mm = String(dateObj.getMonth() + 1).padStart(2, "");
+  const dd = String(dateObj.getDate()).padStart(2, "0");
+  const mm = String(dateObj.getMonth() + 1).padStart(2, "0");
   const yy = String(dateObj.getFullYear());
 
-  const h = String(dateObj.getHours()).padStart(2, "");
-  const m = String(dateObj.getMinutes()).padStart(2, "");
-  const s = String(dateObj.getSeconds()).padStart(2, "");
+  const h = String(dateObj.getHours()).padStart(2, "0");
+  const m = String(dateObj.getMinutes()).padStart(2, "0");
+  const s = String(dateObj.getSeconds()).padStart(2, "0");
   return {
     date: `${dd}-${mm}-${yy}`,
     time: `${h}-${m}-${s}`,
